efae37db196fe58fe2a3d6a7bb7fcd64e7a1df23
SKOT <skotskip@gmail.com>
2023-08-09 16:56:59 +0700

    finished? again

--- src/app/layout.js
+++ src/app/layout.js
@@ -28,8 +28,8 @@ export default function RootLayout({ children }) {
           {children}
           <Footer
             year="2023"
-            fullName="Chayanin Suatap"
-            studentId="650610560"
+            fullName="Korawit Piboon"
+            studentId="650612076"
           />
         </div>
       </body>
